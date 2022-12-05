98ed81b7adbeed3386c5d29c009fa64075f6cc91
Vampiresbane <118571880+Vampiresbane@users.noreply.github.com>
2022-12-05 14:53:51 -0800

    Add files via upload

---
+++ Source/RoadsOfTheRim/Patch_WorldTargeter_StopTargeting.cs
@@ -0,0 +1,21 @@
+﻿using HarmonyLib;
+using RimWorld;
+
+namespace RoadsOfTheRim;
+
+[HarmonyPatch(typeof(WorldTargeter), "StopTargeting")]
+public static class Patch_WorldTargeter_StopTargeting
+{
+    [HarmonyPrefix]
+    public static void Prefix()
+    {
+        if (RoadsOfTheRim.RoadBuildingState.CurrentlyTargeting == null)
+        {
+            return;
+        }
+
+        //RoadsOfTheRim.DebugLog("StopTargeting");
+        RoadsOfTheRim.FinaliseConstructionSite(RoadsOfTheRim.RoadBuildingState.CurrentlyTargeting);
+        RoadsOfTheRim.RoadBuildingState.CurrentlyTargeting = null;
+    }
+}
